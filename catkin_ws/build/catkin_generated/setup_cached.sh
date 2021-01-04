086a14c10880f643700fa10610d743863ea6eebe
kataoka <s17c1034sv@s.chibakoudai.jp>
2021-01-05 00:20:15 +0900

    add catkin_ws

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/tatsumi/robosys2020-kadai2/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PATH="/opt/ros/melodic/bin:/home/tatsumi/anaconda3/condabin:/home/tatsumi/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+export PWD="/home/tatsumi/robosys2020-kadai2/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/tatsumi/robosys2020-kadai2/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/tatsumi/robosys2020-kadai2/catkin_ws/src:$ROS_PACKAGE_PATH"

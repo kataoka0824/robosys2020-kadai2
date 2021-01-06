7867962f88fc1f80433fe5258f83c62d55e52373
kataoka <s17c1034sv@s.chibakoudai.jp>
2021-01-06 11:23:54 +0900

    add GUI

--- catkin_ws/src/kadai2/scripts/sub.py
+++ catkin_ws/src/kadai2/scripts/sub.py
@@ -2,29 +2,49 @@
 import rospy
 from std_msgs.msg import Int32
 from std_msgs.msg import String
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 position=[0,0,0]
 axis="n"
-
+pos_x=[0]
+pos_y=[0]
+pos_z=[0]
 def cb_key(message):
+    global axis,role
     rospy.loginfo(message.data)
     axis=message.data
-    if(message.data!="x" or message.data!="y" or message.data!="z"){
-        axis="n"
-    }
+    print(axis)
+    plt.close()
 def cb_dist(message):
+    rospy.sleep(0.02)
+    global axis,position
+    plt.ion()
+    fig=plt.figure("subscriber")
+    ax=fig.add_subplot(111,projection='3d')
     rospy.loginfo(message.data)
     print(message.data)
-    if (axis="x"){
+    if (axis=="x"):
         position[0]+=message.data
-    }elif(axis="y"){
+    elif(axis=="y"):
         position[1]+=message.data
-    }elif(axis="z"){
+    elif(axis=="z"):
         position[2]+=message.data
-    }
+    pos_x.append(position[0])
+    pos_y.append(position[1])
+    pos_z.append(position[2])
     print(position)
+    print(axis)
+    ax.plot(pos_x,pos_y,pos_z,marker="o")
+    ax.plot(pos_x[0:1],pos_y[0:1],pos_z[0:1],color="r",marker="o")
+    ax.plot(pos_x[-1:],pos_y[-1:],pos_z[-1:],color="c",marker="o")
+    plt.draw()
+    plt.pause(5)
+    #plt.clf()
+
 if __name__ == '__main__':
     rospy.init_node('sub_key')
     sub = rospy.Subscriber('pub_key',String,cb_key)
     sub2 = rospy.Subscriber('pub_dist',Int32,cb_dist)
     rospy.spin()
+
